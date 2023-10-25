6185c08de0687ef34b4a27c2a910bd4a51889be4
AbhishekUnOfficial <avishek.ov@gmail.com>
2023-10-25 20:51:56 +0530

    grid layout design

--- core/models.py
+++ core/models.py
@@ -6,3 +6,6 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     poster = models.ImageField(upload_to="poster")
     slug = AutoSlugField(populate_from="title")
+
+    def __str__(self):
+        return self.title
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import AllMoviesView, SingleMovieView
 
 
 urlpatterns = [
     path("", AllMoviesView.as_view(), name="all-movies"),
     path("<slug:slug>/", SingleMovieView.as_view(), name="single-movie"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
