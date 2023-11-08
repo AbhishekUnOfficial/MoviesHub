7c07cb3e48f1b1de61761fef38e68a777970aa06
AbhishekUnOfficial <avishek.ov@gmail.com>
2023-11-08 20:14:37 +0530

    reupdate

--- MoviesHub/settings.py
+++ MoviesHub/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +23,6 @@ INSTALLED_APPS = [
     "ckeditor",
     "core",
     "autoslug",
-    "crispy_forms",
-    "crispy_tailwind",
     "tailwind",
     "theme",
     "django_browser_reload",
