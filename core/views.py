493e1ee8f2bf832e96ad04fb3556c05fd60bb9e0
AbhishekUnOfficial <avishek.ov@gmail.com>
2023-11-08 21:59:55 +0530

    login and register method update

--- core/views.py
+++ core/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import HttpResponseRedirect, get_object_or_404, render, reverse
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, FormView, ListView
+from django.views.generic import DetailView, FormView, ListView
 from django.db.models import Q
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import Comment, Movie
 from .forms import CommentForm
 
 
+@login_required
 class AllMoviesView(ListView):
     model = Movie
     template_name = "core/all_movies.html"
@@ -14,6 +19,7 @@ class AllMoviesView(ListView):
     ordering = ["-updated"]
 
 
+@login_required
 class SingleMovieView(DetailView):
     model = Movie
     template_name = "core/single_movie.html"
@@ -22,57 +28,56 @@ class SingleMovieView(DetailView):
     slug_url_kwarg = "slug"
 
 
+@login_required
 class CommentView(DetailView):
     model = Comment
     template_name = "core/comment_view.html"
     context_object_name = "comments"
 
 
-def SearchPost(request):
-    if request.method == "POST":
-        searched = request.POST.get("searched", "")
-        if searched:
-            movies = Movie.objects.filter(
-                Q(title__icontains=searched) | Q(description__icontains=searched)
-            )
-        else:
-            movies = Movie.objects.none()
-        return render(
-            request, "core/search_page.html", {"searched": searched, "movies": movies}
+def search_post(request):
+    # Change to GET for search functionality
+    searched = request.GET.get("searched", "")
+
+    movies = (
+        Movie.objects.filter(
+            Q(title__icontains=searched) | Q(description__icontains=searched)
         )
-    else:
-        return render(request, "core/search_page.html", {})
+        if searched
+        else Movie.objects.none()
+    )
+
+    return render(
+        request, "core/search_page.html", {
+            "searched": searched, "movies": movies}
+    )
 
 
-def LoginPage(request):
-    if request.method == "post":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        print(username, password)
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successfully")
+            return redirect("all-movies")
+        else:
+            messages.error(request, "Login failed")
 
     return render(request, "core/login_page.html")
 
 
 def RegisterPage(request):
-    return render(request, "core/register_page.html")
-
-
-# def comment_form(request, slug):
-#     movie = get_object_or_404(Movie, slug=slug)
-#     if request.method == "POST":
-#         commentform = CommentForm(request.POST)
-#         if commentform.is_valid():
-#             new_comment = commentform.save(commit=False)
-#             new_comment.post = movie
-#             new_comment.save()
-#         return HttpResponseRedirect(reverse("single-movie", args=[slug]))
-#     else:
-#         commentform = CommentForm()
-#     return render(
-#         request,
-#         "core/single_movie.html",
-#         {"commentform": commentform, "movie": movie},
-#     )
+    if request.method == "post":
+        form = UserCreationForm()
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully")
+        else:
+            form = UserCreationForm()
+        return render(request, "core/register_page.html", {"form": form})
 
 
 class AddCommentView(FormView):
